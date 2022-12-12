4f4c385dfd67be6404361141461ec05eb3755f16
Julia Gomozova <sunrain@yandex.ru>
2022-12-12 20:32:41 +0300

    add Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.WriteLine("Введите первое число:");
+string numStr1 = Console.ReadLine() ?? "";
+
+if (!int.TryParse(numStr1, out int num1)) 
+{
+    Console.WriteLine("Вы ввели не число");
+    return;
+}
+
+Console.WriteLine("Введите второе число >0 :");
+string numStr2 = Console.ReadLine() ?? "";
+
+if (!int.TryParse(numStr2, out int num2)) 
+{
+    Console.WriteLine("Вы ввели не число");
+    return;
+}
+
+if (num2 > 0)
+{
+Console.WriteLine($"{num1} в степени {num2} = " + Math.Pow(num1, num2));
+}
+else Console.WriteLine("Второе число должно быть >0");
